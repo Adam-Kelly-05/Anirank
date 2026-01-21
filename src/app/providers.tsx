388f7e3cdcd9c49736f0cc20eecfd83a710031fc
Dara <daraorourke209@gmail.com>
2026-01-21 13:52:00 +0000

    testing differnt user pools

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_CE1rF5NoX",
-  client_id: "2a56v5hnl5nn65tho958c2rcj3",
+  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_Sln68srsF",
+  client_id: "1knbc5qbtphhlkv4dqm2humnk2",
   redirect_uri: "https://anirank.ie/",
   response_type: "code",
-  scope: "email openid phone",
+  scope: "phone openid email",
 };
 
 export default function Providers({
