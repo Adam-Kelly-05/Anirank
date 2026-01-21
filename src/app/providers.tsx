adf65a44449cae8781a10ebb5b2cd60cff68b5e7
Dara <daraorourke209@gmail.com>
2026-01-21 13:57:53 +0000

    testing cognito 2.0

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_Sln68srsF",
-  client_id: "1knbc5qbtphhlkv4dqm2humnk2",
+  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_CE1rF5NoX",
+  client_id: "2a56v5hnl5nn65tho958c2rcj3",
   redirect_uri: "https://anirank.ie/",
   response_type: "code",
-  scope: "phone openid email",
+  scope: "email openid phone",
 };
 
 export default function Providers({
