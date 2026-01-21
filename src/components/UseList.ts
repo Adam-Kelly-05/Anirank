d667178437e89746076a678fcefed06ed21c5315
Adam Kelly <adamkellyballintogher@gmail.com>
2026-01-21 02:31:19 +0000

    Updated code to revieve lists, and the example in the interface using a real return from the API

--- src/components/UseList.ts
+++ src/components/UseList.ts
@@ -4,28 +4,32 @@ import * as React from "react";
 import { List } from "@/types/List";
 
 // Fetch lists by userId
-export function useList({ userId }: { userId?: number }) {
-  const [lists, setLists] = React.useState<List[]>([]);
+export function useList({
+  id,
+  idType,
+}: {
+  id?: string | number;
+  idType?: string | number;
+}) {
+  const [list, setLists] = React.useState<List[]>([]);
 
   React.useEffect(() => {
     async function fetchLists() {
-      try {
-        const response = await fetch(
-          `https://p7gfovbtqg.execute-api.eu-west-1.amazonaws.com/prod/list/${userId}`,
-        );
-        const result = await response.json();
-
-        const data = Array.isArray(result)
-          ? result
-          : (result?.Items ?? result?.data ?? []);
-
-        setLists(data);
-      } catch (error) {
-        setLists([]);
+      let url =
+        "https://p7gfovbtqg.execute-api.eu-west-1.amazonaws.com/prod/list/";
+      if (idType === "user") {
+        url += `user/${id}`;
+      } else if (idType === "list") {
+        url += `${id}`;
       }
+
+      const response = await fetch(url);
+      const raw = await response.json();
+      const data = Array.isArray(raw) ? raw : (raw?.Items ?? raw?.data ?? []);
+      setLists(data);
     }
     fetchLists();
-  }, [userId]);
+  }, [id, idType]);
 
-  return lists;
+  return list;
 }
