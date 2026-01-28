eee927dcd85bff4bc3a58244cdbae379f5582f2f
Dara <daraorourke209@gmail.com>
2026-01-28 22:04:17 +0000

    got the sign in fixed

--- src/components/OidcAuthPanel.tsx
+++ src/components/OidcAuthPanel.tsx
@@ -10,7 +10,7 @@ export default function OidcAuthPanel() {
 
   const signOutRedirect = () => {
     const clientId = "2a56v5hnl5nn65tho958c2rcj3";
-    const logoutUri = "https://anirank.ie/";
+    const logoutUri = "https://anirank.ie/profile/";
     const cognitoDomain =
       "https://eu-west-1ce1rf5nox.auth.eu-west-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
