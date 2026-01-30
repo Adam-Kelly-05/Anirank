59e06bfdba0e72462ec0bd1703f20b96557fa376
Adam Kelly <adamkellyballintogher@gmail.com>
2026-01-30 11:39:24 +0000

    Reverted to old user pool

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,11 +4,11 @@ import React from "react";
 import { AuthProvider } from "react-oidc-context";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_O9S7Dru8C",
-  client_id: "7o79k59ebj2jg5s02mio124jc",
+  authority: "https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_CE1rF5NoX",
+  client_id: "2a56v5hnl5nn65tho958c2rcj3",
   redirect_uri: "https://anirank.ie/",
   response_type: "code",
-  scope: "phone openid email",
+  scope: "email openid phone",
 };
 
 export default function Providers({
