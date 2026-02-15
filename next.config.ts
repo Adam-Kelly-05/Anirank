83475cc80c7d9e2335e7188f5fe5766cf934f5eb
breda <breda@DESKTOP-TPQM8M4>
2026-02-15 11:06:07 +0000

    set react strictmode to false

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
 
+  reactStrictMode: false,
+
   images: {
     unoptimized: true,
     remotePatterns: [
