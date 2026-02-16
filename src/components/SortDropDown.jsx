7d0eefe0d2244fc0aad89910aac3ed51ea7b6cc9
breda <breda@DESKTOP-TPQM8M4>
2026-02-16 00:38:58 +0000

    delete pointer

--- src/components/SortDropDown.jsx
+++ src/components/SortDropDown.jsx
@@ -8,9 +8,8 @@ export default function SortDropdown({ selectedSort, onSelectSort }) {
   return (
     <div className="flex justify-center">
       <details className="relative">
-        <summary className="cursor-pointer px-4 py-2 text-xs text-gray-100 border border-blue-500 rounded-full hover:bg-blue-800 transition">
-          <Funnel size={14} className="text-blue-400" />
-          Sort: {selectedSort || "Select"}
+        <summary className="px-4 py-2 text-xs text-gray-100 border border-blue-500 rounded-full hover:bg-blue-800 transition">
+          <Funnel size={14} className="text-blue-400" /> Sort: {selectedSort || "Select"}
         </summary>
 
         <div className="absolute mt-2 bg-[#0a0e1a] border border-blue-500 rounded-lg shadow-lg w-40 z-10">
