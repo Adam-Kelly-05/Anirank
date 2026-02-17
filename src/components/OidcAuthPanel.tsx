199fd4334d304cf8b3b8483b5016e820a8ffa720
PineappleGinge <daraorourke209@gmail.com>
2026-02-17 12:41:48 +0000

    changed domain for logging in and out to localhost for testing and editing (will change back after all is done)

--- src/components/OidcAuthPanel.tsx
+++ src/components/OidcAuthPanel.tsx
@@ -29,7 +29,7 @@ export default function OidcAuthPanel() {
 
   const signOutRedirect = async () => {
     const clientId = "2a56v5hnl5nn65tho958c2rcj3";
-    const logoutUri = "https://anirank.ie/";
+    const logoutUri = "http://localhost:3000/";
     const cognitoDomain = "https://eu-west-1ce1rf5nox.auth.eu-west-1.amazoncognito.com";
     try {
       await auth.removeUser();
