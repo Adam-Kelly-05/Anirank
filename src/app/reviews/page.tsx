b3a170827a8b339fe87d79ac96ab0072f1d32464
Dara <daraorourke209@gmail.com>
2026-03-10 23:24:11 +0000

    alining text on review page

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -22,8 +22,10 @@ export default function AboutPage() {
 
       {/* Most Reviewed Anime Section */}
       <section className="mt-10">
-        <h2 className="text-2xl font-bold mb-4">Most Reviewed Anime</h2>
-        <MostReviewedAnimeSection />
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <h2 className="text-2xl font-bold mb-4">Most Reviewed Anime</h2>
+          <MostReviewedAnimeSection />
+        </div>
       </section>
 
       {/* Reviews Section */}
